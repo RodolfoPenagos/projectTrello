73dabbf52a658f90eb96c6cd26af7d98515cae82
murlock <rodolfopenagosruiz@gmail.com>
2017-12-17 17:16:16 -0600

    anexar MDL

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', murlock: 'hola' });
+  res.render('index', { title: 'Express', murlock: 'hola', pageTitle: 'Trello' });
 });
 
 module.exports = router;
+
