dbda7f842418f5df9cb80c61a03ccdde0b0cad16
murlock <rodolfopenagosruiz@gmail.com>
2017-12-17 20:53:21 -0600

    Inputs to cards & clear form values

--- public/javascripts/add-card.js
+++ public/javascripts/add-card.js
@@ -10,8 +10,23 @@ $(document).ready(function(){
     })
     $('#create-card').click(function(){
         const newCard = $( "#demo-card" ).clone().prop('id', 'newCard')
+
+        let newTitle = $('#title').val()
+
+        let newDesc = $('#description').val()
+
+        let title = newCard.find('#demo-title-container').find('#demo-title')
+        title.html(newTitle)
+
+        
+        let description = newCard.find('#demo-description')
+        description.html(newDesc)
+
         newCard.appendTo( "#card-container" ).fadeIn()
         dialog.close()
+
+        $('#title').val('')
+        $('#description').val('')
     })
 
     $('#cancel').click(function(){
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', murlock: 'hola', pageTitle: 'Trello' });
+  res.render('index', { title: 'Express', murlock: 'hola', pageTitle: 'Maya Scrum Assistant' });
 });
 
 module.exports = router;
