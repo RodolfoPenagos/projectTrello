3e611fe1f5dc0e05379db00bdfe91343cbed4d93
murlock <rodolfopenagosruiz@gmail.com>
2017-12-17 19:15:21 -0600

    Dialog create card & cancel

--- public/javascripts/add-card.js
+++ public/javascripts/add-card.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
+    var dialog = document.querySelector('#card-dialog');
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+
+    }
     $('#add-card').click(function(){
+        dialog.showModal()
+
+    })
+    $('#create-card').click(function(){
         const newCard = $( "#demo-card" ).clone().prop('id', 'newCard')
-
         newCard.appendTo( "#card-container" ).fadeIn()
+        dialog.close()
+    })
+
+    $('#cancel').click(function(){
+        dialog.close()
 
 
-
-        
-    
-    
     })
 })
